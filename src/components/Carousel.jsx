6ec9385feee3afacdd806cfe5301d4fc0b18458b
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-04 11:38:08 +0530

    minor UI updates

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -20,24 +20,26 @@ const Carousel = ({ imageUrls, title, currentIndex, setCurrentIndex }) => {
   }, []);
 
   return (
-    <div className="flex items-center">
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Left}
-        style="text"
-        onClick={handlePrevious}
-      />
-      <img
-        alt={title}
-        className="max-w-56 h-56 max-h-56 w-56"
-        src={imageUrls[currentIndex]}
-      />
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Right}
-        style="text"
-        onClick={handleNext}
-      />
+    <div className="flex flex-col items-center space-y-2">
+      <div className="flex items-center space-x-2">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={handleNext}
+        />
+      </div>
       <div className="flex space-x-1">
         {imageUrls.map((_, index) => (
           <span
