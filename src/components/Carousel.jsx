da64c94bf6bb753469dfca6ac61862e2359ad3ab
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-04 15:02:06 +0530

    Integrated API to display product details

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,19 +1,21 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
 import { Button } from "neetoui";
 
-const Carousel = ({ imageUrls, title, currentIndex, setCurrentIndex }) => {
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const timerRef = useRef(null);
+
+  const handleNext = () =>
+    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
   const resetTimer = () => {
     clearInterval(timerRef.current);
     timerRef.current = setInterval(handleNext, 3000);
   };
-
-  const handleNext = () =>
-    setCurrentIndex(prevIndex => (prevIndex + 1) % imageUrls.length);
 
   const handlePrevious = () => {
     setCurrentIndex(
@@ -29,16 +31,16 @@ const Carousel = ({ imageUrls, title, currentIndex, setCurrentIndex }) => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center space-y-2">
-      <div className="flex items-center space-x-2">
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
         <Button
+          alt={title}
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Left}
           style="text"
           onClick={handlePrevious}
         />
         <img
-          alt={title}
           className="max-w-56 h-56 max-h-56 w-56"
           src={imageUrls[currentIndex]}
         />
@@ -46,7 +48,10 @@ const Carousel = ({ imageUrls, title, currentIndex, setCurrentIndex }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
       <div className="flex space-x-1">
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,42 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Spinner, Typography } from "neetoui";
+import { append, isNotNil } from "ramda";
 
 import Carousel from "./Carousel";
-import { IMAGE_URLS } from "./constants";
+// import { IMAGE_URLS } from "./constants";
 
 const Product = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [product, setProduct] = useState({});
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      );
+      setProduct(response.data);
+      console.log("Product fetched successfully:", response.data);
+    } catch (error) {
+      console.log("An error occurred:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+  const totalDiscounts = mrp - offerPrice;
+  const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
 
   return (
     <div className="px-6 pb-6">
       <div>
-        <p className="py-2 text-4xl font-semibold">Infinix INBOOK</p>
+        <Typography className="py-2 text-4xl font-semibold" style="h1">
+          {name}
+        </Typography>
         <hr className="border-2 border-black" />
       </div>
       <div className="mt-6 flex gap-4">
         <div className="w-2/5">
-          <Carousel
-            currentIndex={currentIndex}
-            imageUrls={IMAGE_URLS}
-            setCurrentIndex={setCurrentIndex}
-            title="Infinix Inbook"
-          />
-          {/* <div className="mt-4 flex justify-center">
-            {IMAGE_URLS.map((_, index) => (
-              <span
-                className="neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border"
-                key={index}
-              />
-            ))}
-          </div> */}
+          <div className="flex justify-center gap-16">
+            {isNotNil(imageUrls) ? (
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+            ) : (
+              <img alt={name} className="w-48" src={imageUrl} />
+            )}
+          </div>
         </div>
         <div className="w-3/5 space-y-4">
-          <p>
-            Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year
-            Warranty.
-          </p>
-          <p>MRP: $395.97</p>
-          <p className="font-semibold">Offer price: $374.43</p>
-          <p className="font-semibold text-green-600">6% off</p>
+          <Typography>{description}</Typography>
+          <Typography>MRP: {mrp}</Typography>
+          <Typography className="font-semibold">
+            Offer price: {offerPrice}
+          </Typography>
+          <Typography className="font-semibold text-green-600">
+            {discountPercentage}% off
+          </Typography>
         </div>
       </div>
     </div>
