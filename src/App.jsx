0d0764833e468974e777cec3e2b6110144d3bd42
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-06 12:49:51 +0530

    Added cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Cart from "components/Cart";
 import { PageNotFound } from "components/commons";
+import Product from "components/Product";
+import ProductList from "components/ProductList";
 import { Route, Switch, Redirect } from "react-router-dom";
 import routes from "routes";
 
 import "./App.css";
-import Product from "./components/Product";
-import ProductList from "./components/ProductList";
 
 const App = () => (
   <Switch>
+    <Route exact component={Cart} path={routes.cart} />
     <Route exact component={Product} path={routes.products.show} />
     <Route exact component={ProductList} path={routes.products.index} />
     <Redirect exact from={routes.root} to={routes.products.index} />
