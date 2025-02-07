3ca2befcc0cbe00f4926666577f92efb632203cb
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-07 20:24:33 +0530

    Created React query custom hook to implement parallel queries

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
-
-import productsApi from "apis/products";
 import { PageLoader, Header } from "components/commons";
 import { cartTotalOf } from "components/utils";
+import { useFetchCartProducts } from "hooks/reactQuery/useProductsApi";
 import i18n from "i18next";
-import { NoData, Toastr } from "neetoui";
+import { NoData } from "neetoui";
 import { isEmpty, keys } from "ramda";
+import { useTranslation } from "react-i18next";
 import useCartItemsStore from "stores/useCartItemsStore";
 import withTitle from "utils/withTitle";
 
@@ -15,55 +14,23 @@ import ProductCard from "./ProductCard";
 import { MRP, OFFER_PRICE } from "../constants";
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { t } = useTranslation();
 
-  const { cartItems, setSelectedQuantity } = useCartItemsStore.pick();
-  const slugs = keys(cartItems);
+  const slugs = useCartItemsStore(store => keys(store.cartItems));
+
+  const { data: products = [], isLoading } = useFetchCartProducts(slugs);
 
   const totalMrp = cartTotalOf(products, MRP);
   const totalOfferPrice = cartTotalOf(products, OFFER_PRICE);
-
-  const fetchCartProducts = async () => {
-    try {
-      const responses = await Promise.all(
-        slugs.map(slug => productsApi.show(slug))
-      );
-
-      setProducts(responses);
-      responses.forEach(({ availableQuantity, name, slug }) => {
-        if (availableQuantity >= cartItems[slug]) return;
-
-        setSelectedQuantity(slug, availableQuantity);
-        if (availableQuantity === 0) {
-          Toastr.error(
-            `${name} is no longer available and has been removed from cart`,
-            {
-              autoClose: 2000,
-            }
-          );
-        }
-      });
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log("An error occurred:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCartProducts();
-  }, [cartItems]);
 
   if (isLoading) return <PageLoader />;
 
   if (isEmpty(products)) {
     return (
       <>
-        <Header title="My Cart" />
+        <Header title={t("cart.title")} />
         <div className="flex h-screen items-center justify-center">
-          <NoData title="Your cart is empty!" />
+          <NoData title={t("cart.empty")} />
         </div>
       </>
     );
@@ -71,7 +38,7 @@ const Cart = () => {
 
   return (
     <>
-      <Header title="My Cart" />
+      <Header title={t("cart.title")} />
       <div className="mt-10 flex justify-center space-x-10">
         <div className="w-1/3 space-y-5">
           {products.map(product => (
--- src/components/commons/index.js
+++ src/components/commons/index.js
@@ -2,6 +2,14 @@ import AddToCart from "./AddToCart";
 import Header from "./Header";
 import PageLoader from "./PageLoader";
 import PageNotFound from "./PageNotFound";
+import ProductQuantity from "./ProductQuantity";
 import TooltipWrapper from "./TooltipWrapper";
 
-export { Header, PageNotFound, PageLoader, AddToCart, TooltipWrapper };
+export {
+  Header,
+  PageNotFound,
+  PageLoader,
+  AddToCart,
+  TooltipWrapper,
+  ProductQuantity,
+};
