99fd61c5e3446321cc76854991215a8383465029
Suman Sahoo <suman.sahoo@bigbinary.com>
2025-02-09 16:23:25 +0530

    Added submission handler to forms

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,6 +1,23 @@
 import axios from "axios";
+import { t } from "i18next";
 import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { Toastr } from "neetoui";
 import { evolve } from "ramda";
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+const showErrorToastr = error => {
+  if (error.message === t("error.networkError")) {
+    Toastr.error(t("error.noInternetConnection"));
+  } else if (error.response?.status !== 404) {
+    Toastr.error(error);
+  }
+};
 
 const setHttpHeaders = () => {
   axios.defaults.headers = {
@@ -14,11 +31,19 @@ const transformResponseKeysToCamelCase = response => {
 };
 
 const responseInterceptors = () => {
-  axios.interceptors.response.use(response => {
-    transformResponseKeysToCamelCase(response);
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
 
-    return response.data;
-  });
+      return response.data;
+    },
+    error => {
+      showErrorToastr(error);
+
+      return Promise.reject(error);
+    }
+  );
 };
 
 const requestInterceptors = () => {
